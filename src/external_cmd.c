2716dda4bd08d42a6030713d4e7fd8c2c3a08fa6
Juliana Bonardi <julicbonardi@hotmail.com>
2025-05-04 23:55:07 -0300

    External command working with env PATH

--- src/external_cmd.c
+++ src/external_cmd.c
@@ -1,13 +1,68 @@
 #include "../includes/minishell.h"
 
-char *get_path(t_command *cmd)
+static void  free_dir(char ***dir)
+{
+  int i;
+
+  i = 0;
+  if (!dir || !*dir)
+    return ;
+  while ((*dir)[i])
+  {
+    free((*dir)[i]);
+    i++;
+  }
+  free(*dir);
+  *dir = NULL;
+}
+
+static char *get_env_path(t_command *cmd, char  ***dir)
 {
   char  *path;
+  char  *temp;
+  int   i;
 
+  i = 0;
+  while ((*dir)[i])
+  {
+    temp = ft_strjoin((*dir)[i], "/");
+    if (!temp)
+      break ;
+    path = ft_strjoin(temp, cmd->command_name);
+    free(temp);
+    if (!path)
+      break ;
+    if (access(path, X_OK) == 0)
+    {
+      free_dir(dir);
+      return (path);
+    }
+    free(path);
+    i++;
+  }
+  free_dir(dir);
+  return (NULL);
+}
+
+static char *get_path(t_command *cmd)
+{
+  char  *env_path;
+  char  *path;
+  char  **dir;
+
+  dir = NULL;
   if (ft_strchr(cmd->command_name, '/'))
     path = cmd->command_name;
   else
-    path = ft_strjoin("/bin/", cmd->command_name);
+  {
+    env_path = getenv("PATH");
+    if (!env_path)
+      return (NULL);
+    dir = ft_split(env_path, ':');
+    if (!dir)
+      return (NULL);
+    path = get_env_path(cmd, &dir);
+  }
   if (access(path, X_OK) != 0)
     perror("Command not found or not executable");
   return (path);
@@ -30,5 +85,6 @@ void  exec_external_cmd(t_command *cmd)
     waitpid(pid, NULL, 0);
   else
     perror("Error fork");
-  free(path);
+  if (!ft_strchr(cmd->command_name, '/'))
+    free(path);
 }

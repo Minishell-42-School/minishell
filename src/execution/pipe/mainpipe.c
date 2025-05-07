91104a9966f922089e8f8fb85e02289c9191bce7
Juliana Bonardi <julicbonardi@hotmail.com>
2025-05-07 18:07:18 -0300

    Updating

--- src/execution/pipe/mainpipe.c
+++ src/execution/pipe/mainpipe.c
@@ -13,6 +13,19 @@ t_command	*make_cmd(char *name, char **args)
 	return cmd;
 }
 
+t_command	*make_cmd_red(char *name, char **args, t_redirections *redir)
+{
+	t_command *cmd = malloc(sizeof(t_command));
+	cmd->command_name = name;
+	cmd->args = args;
+	cmd->args_count = 0;
+	cmd->redirs = redir;
+	cmd->nbr_env_var = 0;
+	cmd->expand_var = NULL;
+	cmd->next = NULL;
+	return cmd;
+}
+
 int	main(void)
 {
 	char *args1[] = {"ls", NULL, NULL};
@@ -24,6 +37,14 @@ int	main(void)
 
   char *args8[] = {"cat", "/etc/passwd", NULL};
   char *args9[] = {"head", "-n", "3", NULL};
+
+  // Pipe with redir
+  // grep "user" < entrada.txt | uniq >> saida.txt
+  char *args11[] = {"grep", "user", NULL};
+  t_redirections *redir1 = {R_IN, "entrada.txt", NULL};
+  char *args12[] = {"uniq", NULL, NULL};
+  t_redirections *redir2 = {R_DELIMITER, "saida.txt", NULL};
+
 
   t_command *cmd1 = make_cmd("ls", args1);
 	t_command *cmd2 = make_cmd("wc", args2);
@@ -39,20 +60,26 @@ int	main(void)
   t_command *cmd9 = make_cmd("head", args9);
   t_command *cmd10 = make_cmd("wc", args2);
 
+  t_command *cmd11 = make_cmd_red("grep", args11, redir1);
+  t_command *cmd12 = make_cmd_red("uniq", args12, redir2);
+
 	cmd1->next = cmd2;
   cmd3->next = cmd4;
   cmd5->next = cmd6;
   cmd6->next = cmd7;
   cmd8->next = cmd9;
   cmd9->next = cmd10;
+  cmd11->next = cmd12;
 
-  exec_pipeline(cmd1);
+  // exec_pipeline(cmd1);
+  // printf("\n --------------- \n\n");
+	// exec_pipeline(cmd3);
+  // printf("\n --------------- \n\n");
+  // exec_pipeline(cmd5);
+  // printf("\n --------------- \n\n");
+  // exec_pipeline(cmd8);
   printf("\n --------------- \n\n");
-	exec_pipeline(cmd3);
-  printf("\n --------------- \n\n");
-  exec_pipeline(cmd5);
-  printf("\n --------------- \n\n");
-  exec_pipeline(cmd8);
+  exec_pipeline(cmd11);
 
 	free(cmd1);
 	free(cmd2);
@@ -64,5 +91,7 @@ int	main(void)
   free(cmd8);
   free(cmd9);
   free(cmd10);
+  free(cmd11);
+  free(cmd12);
 	return 0;
 }

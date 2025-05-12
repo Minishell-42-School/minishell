c02b00878479a51a8343159a5b7db73ae4be414b
Juh-B <jcosta-b@c1r3p3.42sp.org.br>
2025-05-12 18:32:23 -0300

    Redir out and append

--- src/execution/redirections/redir.c
+++ src/execution/redirections/redir.c
@@ -6,7 +6,7 @@
 /*   By: jcosta-b <jcosta-b@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/08 18:10:40 by jcosta-b          #+#    #+#             */
-/*   Updated: 2025/05/12 17:59:02 by jcosta-b         ###   ########.fr       */
+/*   Updated: 2025/05/12 18:29:42 by jcosta-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,25 +16,40 @@ static void	child_proc(t_command *cmd)
 {
 	int	fd;
 	char	*path;
-	// int	fd_backup;
+
 	// if (!cmd->redirs->next) // Apenas 1 redir
 	// {
 	if (cmd->redirs->type == R_OUT) // >
-	{
+		fd = open(cmd->redirs->filename, O_CREAT | O_WRONLY | O_TRUNC, 0644);
+	else if (cmd->redirs->type == R_APPEND) // >>
 		fd = open(cmd->redirs->filename, O_CREAT | O_WRONLY | O_APPEND, 0644);
-		if (fd == -1)
-		{
-			printf("Error for open the file");
-			return ;
-		}
+	else //if (cmd->redirs->type == R_IN) // <
+		fd = open(cmd->redirs->filename, O_RDONLY, 0644);
+
+	if (fd == -1)
+	{
+		printf("Error for open the file");
+		return ;
+	}
+	if (cmd->redirs->type == R_OUT || cmd->redirs->type == R_APPEND)
+	{
 		if (dup2(fd, STDOUT_FILENO) == -1)
 		{
 			printf("Error - dup");
 			close(fd);
 			return ;
 		}
-		close(fd);
 	}
+	else
+	{
+		if (dup2(fd, STDIN_FILENO) == -1)
+		{
+			printf("Error - dup");
+			close(fd);
+			return ;
+		}
+	}
+	close(fd);
 	path = get_path(cmd);
 	execve(path, cmd->args, NULL);
 	perror("execve");

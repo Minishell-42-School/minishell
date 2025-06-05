aaadbd8283240f56550798d391e7277f5968fc1e
Eduardo Keller Da Silva <ekeller-@c1r5p1.42sp.org.br>
2025-06-05 11:49:50 -0300

    ready to merge with main

--- src/builtins/echo_builtin.c
+++ src/builtins/echo_builtin.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   echo_builtin.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jcosta-b <jcosta-b@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ekeller-@student.42sp.org.br <ekeller-@    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/28 13:47:28 by ekeller-@st       #+#    #+#             */
-/*   Updated: 2025/06/03 17:02:12 by jcosta-b         ###   ########.fr       */
+/*   Updated: 2025/06/05 11:44:51 by ekeller-@st      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	exec_echo_builtin(t_command *cmd)
 	int	newline;
 
 	i = 1;
-	newline = 1;
+	newline = 1;	
 	if (cmd->args[1] && ft_strcmp(cmd->args[1], "-n") == 0)
 	{
 		newline = 0;
